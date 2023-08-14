06c4698119859470bf66c8d730f06cd4670a93b9
hibaasim <hiba.asim10@gmail.com>
2023-08-14 15:51:05 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 'a';
+
+	while (i <= 'z' && i != 'e' && i != 'q')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
