e05d35254ed8d99d571dc1f3ac3ceab952a85e0d
hibaasim <hiba.asim10@gmail.com>
2023-08-14 15:11:27 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 'a';
+	while ( i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	return (0);
+}
+
