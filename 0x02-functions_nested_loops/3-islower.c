2dd67c410d3443cdf19dc33720a800bc8caa090d
hibaasim <hiba.asim10@gmail.com>
2023-08-16 11:57:30 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,3 @@
-#include "main.h"
-#include <ctype.h>
-
 /**
  * _islower - Entry point
  *
@@ -8,7 +5,7 @@
  */
 int _islower(int c)
 {
-	if (islower(c) == 1)
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	} else 
