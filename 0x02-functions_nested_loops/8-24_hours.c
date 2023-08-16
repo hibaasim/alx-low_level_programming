48af69ae806afe5fb5262f9f70dc2b8aa1228254
hibaasim <hiba.asim10@gmail.com>
2023-08-16 14:58:30 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,24 +6,18 @@
  */
 void jack_bauer(void)
 {
-	int h, r, m, n;
+	int h, m;
 
-	for (h = 0; h <= 2; h++)
+	for (h = 0; h <= 23; h++)
 	{
-		for (r = 0; r <= 3; r++)
-		{
-			for (m = 0; m <= 5; m++)
-			{
-				for (n = 0; n <= 9; n++)
-				{
-					_putchar(h + '0');
-					_putchar(r + '0');
-					_putchar(':');
-					_putchar(m + '0');
-					_putchar(n + '0');
-					_putchar('\n');
-				}
-			}
+		for (m = 0; m <= 59; m++)
+		{			
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar(':');
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar('\n');
 		}
 	}
 }
