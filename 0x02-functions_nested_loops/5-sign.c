ecbde570e0a9f690d4eab119833e993070cb930f
hibaasim <hiba.asim10@gmail.com>
2023-08-16 12:44:27 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - shows the sign of the given number
+ * @n: input number
+ *
+ * Return: 1 if +ve, 0 if zero and -1 if -ve
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	} else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
