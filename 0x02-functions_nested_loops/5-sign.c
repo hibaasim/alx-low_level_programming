ddc54cbbab30902d3e78b1f70ceed40c7e1fdfe7
hibaasim <hiba.asim10@gmail.com>
2023-08-16 13:06:17 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,12 +12,15 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
-	} else if (n < 0)
+	}
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
-	} else
+	}
+	else
 	{
 		_putchar('0');
+		return (0);
 	}
 }
