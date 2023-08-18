75135a1990193cb1b0a38e82296bdcdd99db7f7b
hibaasim <hiba.asim10@gmail.com>
2023-08-17 18:57:59 -0700

    some tasks to 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints from 0 to 14 x10
+ *
+ */
+void more_numbers(void)
+{
+	int j, c;
+
+	for (j = 0; j <= 10; j++)
+	{
+		for (c = 0; c < 14; c++)
+		{
+			if (c > 9)
+				_putchar(c / 10 + 48);
+			_putchar((c % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
