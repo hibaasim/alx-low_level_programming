d6fa7a67771e20fc756a32ca4382541be6d5e0a0
hibaasim <hiba.asim10@gmail.com>
2023-08-20 14:58:39 -0700

    advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful %d - Dora Korpar, 2015-10-19\n" , '"');
+	fprintf(stderr, "and that piece of art is useful %c - Dora Korpar, 2015-10-19\n", '"');
 	return (1);
 }
