6bafff1b1ffeecd653ff17aafa84e9593c059647
hibaasim <hiba.asim10@gmail.com>
2023-08-21 14:40:29 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets pointer to point to 98
+ * @*n: pointer to n
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a pointers and arrays project
