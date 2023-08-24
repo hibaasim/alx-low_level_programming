8d9d6cedc349f843462b32c61220d98c3fdbbf5d
hibaasim <hiba.asim10@gmail.com>
2023-08-24 13:50:16 -0700

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: destination string
+ * @src: string to be concatenated
+ *
+ * Return: the concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int d = 0, s = 0, i;
+
+	while (*dest)
+		d++;
+	while (*src)
+		s++;
+
+	for (i = 0; i <= s; i++)
+	{
+		dest[d++] = src[i];
+	}
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This project is more practice on pointers, arrays and strings.
