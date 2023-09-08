dad25e79e8024f5031dc0221a7b12b1c106a8db2
hibaasim <hiba.asim10@gmail.com>
2023-09-07 18:15:33 -0700

    0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocated memory using malloc
+ * @b: size of memory
+ *
+ * Return: pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *s;
+       
+	s = malloc(b);
+	if (s == NULL)
+		exit(98);
+	return (s);
+}
