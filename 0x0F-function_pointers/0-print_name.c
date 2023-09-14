df45b49edb166df64b6d9af70798f74b69a35a51
hibaasim <hiba.asim10@gmail.com>
2023-09-13 18:26:36 -0700

    0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints th names in funcions
+ * @name: pointer to name
+ * @f: pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+	return;
+}
