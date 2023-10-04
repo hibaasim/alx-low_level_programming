9cabeb173421d1277fd9edebbc590d40d0203866
hibaasim <hiba.asim10@gmail.com>
2023-10-04 07:02:38 -0700

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,9 +15,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-		return(NULL);
+		return (NULL);
 	p = malloc(sizeof(char) * size);
+	if (p == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		p[i] = c;
-	return(p);
+	return (p);
 }
