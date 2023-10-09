c73d03be4ee300de9b89d1fec6a9c61c25dbc9b4
hibaasim <hiba.asim10@gmail.com>
2023-10-08 17:46:39 -0700

    3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	pt = malloc (sizeof(int) * ((max - min) + 1));
+	pt = malloc(sizeof(int) * ((max - min) + 1));
 	if (pt == NULL)
 		return (NULL);
 	for (i = 0; min <= max; i++)
