6df6b8f5de8bf01b8a703a8d543ef592f7dfbc5b
hibaasim <hiba.asim10@gmail.com>
2023-10-15 16:10:36 -0700

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct name
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: name of owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
