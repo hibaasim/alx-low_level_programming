2353461aed65cda1eaa58aa39fc878ca9ca66f33
hibaasim <hiba.asim10@gmail.com>
2023-10-19 19:54:13 -0700

    0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints list elements
+ * @h: head of list
+ *
+ * Return: no of list elments
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf"([0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
