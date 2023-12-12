81e3889d713affb23047ce97548a04326a490b62
hibaasim <hiba.asim10@gmail.com>
2023-12-11 19:14:10 -0800

    task 0

---
+++ 0x18-dynamic_libraries/abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - gets absolue value of a number
+ * @j: input number
+ *
+ * Return: value of j
+ */
+int _abs(int j)
+{
+	if (j >= 0)
+		return (j);
+	else
+		return (-j);
+}
