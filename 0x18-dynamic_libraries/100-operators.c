283a4af38a8aecf502dd0528b9da93723e3d44f0
hibaasim <hiba.asim10@gmail.com>
2023-12-15 09:57:30 -0800

    advanced 1

---
+++ 0x18-dynamic_libraries/100-operators.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - adds two ints
+ * @a: first num
+ * @b: second num
+ *
+ * Return: result of addition
+ */
+int add(int a, int b)
+{
+	return ((a) + (b));
+}
+
+/**
+ * sub - subtracts two ints
+ * @a: first num
+ * @b: second num
+ *
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return ((a) - (b));
+}
+
+/**
+ * mul - multiplies two ints
+ * @a: first num
+ * @b: second num
+ *
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return ((a) * (b));
+}
+
+/**
+ * div - divides two ints
+ * @a: first num
+ * @b: second num
+ *
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return ((a) / (b));
+}
+
+/**
+ * mod - gives remainder of division beween ints
+ * @a: first num
+ * @b: second num
+ *
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return ((a) % (b));
+}
