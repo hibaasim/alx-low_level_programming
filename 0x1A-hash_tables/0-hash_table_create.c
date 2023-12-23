3b10d7a3d72c57c883de7d654b252e6b786fef60
hibaasim <hiba.asim10@gamil.com>
2023-12-23 04:13:52 +0200

    zero

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,12 +12,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	table = malloc(sizeof(hash_table_t));
-	if (!table)
+	if (table == NULL)
 		return (NULL);
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t) * size);
 	if (table->array == NULL)
-		free(table);
 		return (NULL);
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
