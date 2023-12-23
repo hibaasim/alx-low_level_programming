79d4b91e0bc855ff83c25e9f8028f0d20d9e66e9
hibaasim <hiba.asim10@gamil.com>
2023-12-23 04:44:09 +0200

    two

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives key index
+ * @key: pointer to key
+ * @size: size of array
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    unsigned long int index;
+
+    index = hash_djb2(key) % size;
+    return (index);
+}
