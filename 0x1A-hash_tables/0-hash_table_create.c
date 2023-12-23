8f8b8194614b9b482721e3d981a59b2f07323503
hibaasim <hiba.asim10@gamil.com>
2023-12-23 04:10:12 +0200

    zero

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,1 +1,25 @@
-#includ
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: hash tale size
+ *
+ * Return: address of hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	unsigned long int i;
+
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t) * size);
+	if (table->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+	return (table);
+
+}
