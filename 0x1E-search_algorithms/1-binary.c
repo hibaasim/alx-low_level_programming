619759bb376eeaa5281733c32b42864ffd7df676
hibaasim <hiba.asim10@gmail.com>
2024-07-17 19:54:42 -0700

    task-0

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value using Binary Search
+ *
+ *@array: input array
+ *@size: number of elements in array
+ *@value: value to search in
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+int binary_search(int *array, size_t size, int value)
+{
+}
