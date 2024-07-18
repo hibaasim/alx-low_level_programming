db001f923e2b0a60b961a8392635487bae7b1c26
hibaasim <hiba.asim10@gmail.com>
2024-07-18 12:41:31 -0700

    task-1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,11 +11,32 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int l, m, h;
+	int l = 0, m, h = (size - 1);
 	int i;
 
 	if (array == NULL)
 	{
 		return (-1);
 	}
+
+	while(l <= h)
+	{
+		m = (l + h) / 2;
+		
+		printf("Searching in array: ");
+		for (i = l; i <= h; i++)
+		{
+			if (i == h)
+				printf("%i\n", array[i]);
+			else
+				printf("%i%s", array[i],",");
+		}
+		if (array[m] < value)
+			l = m + 1;
+		else if (array[m] > value)
+			h = m - 1;
+		else
+			return (m);
+	}
+	return (-1);
 }
